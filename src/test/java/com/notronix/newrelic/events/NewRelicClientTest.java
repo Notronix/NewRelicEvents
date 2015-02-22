54adac2bdb7d5f792797d78ddac1671acad0be07
Clint Munden <clint.munden@verafin.com>
2015-02-22 12:32:15 -0330

    Fixed error with determining invalid event types.

---
+++ src/test/java/com/notronix/newrelic/events/NewRelicClientTest.java
@@ -0,0 +1,36 @@
+package com.notronix.newrelic.events;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.fail;
+
+@RunWith(JUnit4.class)
+public class NewRelicClientTest
+{
+    @Test(expected = APIViolationException.class)
+    public void testInvalidEventType() throws APIViolationException
+    {
+        NewRelicEvent event = new NewRelicEvent() {
+            @Override
+            public String getEventType()
+            {
+                return "Bad.Event";
+            }
+        };
+
+        NewRelicClient client = new NewRelicClient();
+        client.setInsertKey("test");
+        client.setAccountId(0);
+
+        try
+        {
+            client.submit(event);
+        }
+        catch (IllegalStateException | NewRelicLoggingException | NullPointerException e)
+        {
+            fail("Should not have thrown any other exception than an APIViolationException.");
+        }
+    }
+}
